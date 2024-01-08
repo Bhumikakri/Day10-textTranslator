cc4cae09470db7ac7fb7102dce3942d6b9860665
Bhumika Kumari <128302166+Bhumikakri@users.noreply.github.com>
2024-01-08 14:13:02 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Main from './Comp/Main';
 function App() {
   return (
     <div className="App">
-      <h1>Text translator</h1>
+      <h1> <img src='https://i.pinimg.com/originals/79/5d/43/795d43ef6591d870f9ee03dc6a8e1218.png'/>Text translator</h1>
       <Main />
     </div>
   );
